665cd92d2f1dcf13694d55642cf267ed9020ca7f
sojib600 <sojibdas400@gmail.com>
2023-06-22 18:58:42 +0600

    comp for task decorator

--- app/components/demo-two.js
+++ app/components/demo-two.js
@@ -1,6 +1,5 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 import { task } from 'ember-concurrency';
 import getRandomNumber from '../utils/get-random-number';
 
