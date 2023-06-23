409d6b6113236f77a79246da59adc11570a827f5
sojib600 <sojibdas400@gmail.com>
2023-06-23 15:39:30 +0600

    cancel task

--- app/components/demo-four.js
+++ app/components/demo-four.js
@@ -5,12 +5,20 @@ import { task } from 'ember-concurrency';
 import getRandomNumber from '../utils/get-random-number';
 
 export default class DemoOneComponent extends Component {
+  @tracked randomNumber;
+
   @action
   async getRandNumber() {
     this.getNumberTask.perform();
   }
 
-  @task({ keepLatest: true }) *getNumberTask() {
-    return yield getRandomNumber();
+  @task({ keepLatest: true })
+  *getNumberTask() {
+    return (this.randomNumber = yield getRandomNumber());
+  }
+
+  @action
+  handleCancel() {
+    this.getNumberTask.cancelAll();
   }
 }
